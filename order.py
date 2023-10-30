fa50959f48258258cc4c5950b6ab1e1980f16c12
NormaArdhitaVidyiawati <147597223+NormaArdhitaVidyiawati@users.noreply.github.com>
2023-10-30 22:27:44 +0700

    Update order.py

--- order.py
+++ order.py
@@ -19,4 +19,30 @@ else:
     total_hrga = 0
     print("Sorry, your fav topping was sold out/nothing")
 
+#showing and inputing crust menu
+print("Our Crust Menu, Pelase choose one ")
+print("original,stuffedcrust cheese,stuffedcrust sausage")
+crustPizza = input("Choose your fav crust : ")
+if cruzPizza == "original":
+    total_hrga += 0
+elif cruzPizza == "stuffedcrust cheese":
+       total_hrga += 20000
+elif crustPizza == "stuffedcrust sausage":
+        total_hrga += 10000
+else:
+        print("Sorry, Your fav Crust was sold out")
+
+#showing and inputing pizza size menu
+print("Our Size Menu, Pelase choose one ")
+print("regular,medium,large")
+size = input("Choose your fav size : ")
+if size == "regular":
+    total_hrga += 0
+elif size == "medium":
+      total_hrga += 20000
+elif size == "large":
+        total_hrga += 40000
+else:
+    print("Sorry, Your fav Size was sold out")
+
 #contoh
